64e15194696d46ac16a6e7faff65ce048f081862
Currency-Converter <z8784507@gmail.com>
2026-02-26 13:55:30 -0800

    create a middleware and a function to handle possible errors

--- api/index.js
+++ api/index.js
@@ -1,24 +1,32 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
-import { log } from 'node:console';
+import { log } from "node:console";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(()=>{
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
     console.log("Connected to MongoDB");
-}).catch((err)=>{
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 
-const app= express();
+const app = express();
 app.use(express.json());
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 
-app.listen(3000, ()=> {
-    console.log("Server is running on port 3000");
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server error";
+  return res.status(statusCode).json({ success: false, statusCode, message });
 });
 
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
