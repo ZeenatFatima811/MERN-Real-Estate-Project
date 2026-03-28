69fb9fadd500f9d62ab3bbfab3fe8f83de433c82
Currency-Converter <z8784507@gmail.com>
2026-03-28 12:30:09 +0500

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800 ">About Prime Heaven</h1>
+      <p className="mb-4 text-slate-700">
+        Prime Heaven is a premier real estate destination dedicated to turning
+        your homeownership dreams into reality. We are a trusted name in the
+        property market, specializing in the sale and purchase of residential
+        plots, luxury apartments, and high-potential commercial properties. Our
+        goal is to provide more than just a transaction; we aim to find you a
+        space that offers both a peaceful lifestyle and a secure, profitable
+        investment for the future.
+      </p>
+      <p className="mb-4 text-slate-700">
+        At Prime Heaven, we believe that buying property is one of life’s most
+        significant decisions. That is why our team provides expert guidance
+        based on deep market insights and data-driven analysis. We prioritize
+        absolute transparency and integrity in every deal, ensuring that every
+        detail provided on our platform is verified. This commitment allows you
+        to make informed, high-confidence decisions without the stress of hidden
+        complexities.
+      </p>
+      <p className="mb-4 text-slate-700">
+        We are passionate about building lasting relationships with our clients,
+        whether you are a first-time homebuyer or a seasoned investor. Our
+        mission is to simplify the real estate journey by offering personalized
+        services tailored to your specific needs. At Prime Heaven, we don’t just
+        list properties—we help you secure your future by connecting you with
+        the best locations and market rates available.
+      </p>
+    </div>
+  );
 }
